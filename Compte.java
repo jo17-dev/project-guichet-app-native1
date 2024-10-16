c6a2e1c5fa1fccea4a05e48a99d67b380569eaca
Allag Sami Riadh <2372559@crosemont.qc.ca>
2024-10-16 09:46:23 -0400

    Ajout des variables et constructeur pour la classe Compte

--- Compte.java
+++ Compte.java
@@ -1,3 +1,15 @@
 public class Compte {
-    
+    private int numeroCompte;
+    private int codeClient;
+    private double soldeCompte;
+    private double retraitMaximum;
+    private double montantTransfertMaximum;
+
+    public Compte(int numeroCompte, int codeClient, double soldeCompte, double retraitMaximum, double montantTransfertMaximum) {
+        this.numeroCompte = numeroCompte;
+        this.codeClient = codeClient;
+        this.soldeCompte = soldeCompte;
+        this.retraitMaximum = retraitMaximum;
+        this.montantTransfertMaximum = montantTransfertMaximum;
+    }
 }
