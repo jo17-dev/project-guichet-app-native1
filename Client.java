56e6dcc5da5437139ebd3fb7e2d92c920ef28e41
jo17-dev <procompter1@gmail.com>
2024-10-16 10:36:39 -0400

    touts les fichers de classes ajoutés

---
+++ Client.java
@@ -0,0 +1,16 @@
+/**
+ * Client
+ */
+public class Client {
+    private String codeClient;
+    private String nom;
+    private String prenom;
+    private String telephone;
+    private String courriel;
+    private int numeroNIP;
+
+    // implementer le constructeur(ajouter ses parametres) et les autresz methodes
+    public Client(){
+        
+    }
+}
