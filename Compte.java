c3c762b3365226c8abdffd7e76bf0f521575e4be
jo17-dev <procompter1@gmail.com>
2024-10-22 21:20:06 -0400

    classe compte implementée

--- Compte.java
+++ Compte.java
@@ -3,13 +3,45 @@ public class Compte {
     private int codeClient;
     private double soldeCompte;
     private double retraitMaximum;
-    private double montantTransfertMaximum;
+    private double montantTransfertMaximum; // nécéssaire ?
 
     public Compte(int numeroCompte, int codeClient, double soldeCompte, double retraitMaximum, double montantTransfertMaximum) {
         this.numeroCompte = numeroCompte;
         this.codeClient = codeClient;
         this.soldeCompte = soldeCompte;
-        this.retraitMaximum = retraitMaximum;
+        this.retraitMaximum = retraitMaximum; // doit forcement etre initialisé avec 1000 ici
         this.montantTransfertMaximum = montantTransfertMaximum;
+    }
+
+    public void retrait(double montant){
+        if(montant % 10 == 0 || montant > retraitMaximum){
+            System.out.println("Impossible de retirer les montant de plus de 1000 $");
+        }else if(montant <= soldeCompte){
+            soldeCompte -= montant;
+        }
+    }
+
+    public void depot(double montant){
+        if(montant > 0){
+            soldeCompte += montant;
+        }else{
+            System.out.println("Impossible de faire le dépot d'un montant négatif");;
+        }
+    }
+
+    public int getCodeClient(){
+        return codeClient;
+    }
+
+    public int getNumeroCompte(){
+        return numeroCompte;
+    }
+
+    public double getSolde(){
+        return soldeCompte;
+    }
+
+    public String toString(){
+        return "Compte ( NumeroCompte: "+ numeroCompte + ", codeClient: " + codeClient + ", soldeCompte: "+ soldeCompte + ", retraitMaximum: "+ retraitMaximum + ", montantTransfertMaximum: " + montantTransfertMaximum + " )" ;
     }
 }
