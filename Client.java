deba78bb941404bc0cf676aa3e42acbd6084388f
2372559 <2372559@crosemont.qc.ca>
2024-10-24 19:41:04 -0400

    Implémentation de la classe Client

--- Client.java
+++ Client.java
@@ -9,8 +9,33 @@ public class Client {
     private String courriel;
     private int numeroNIP;
 
-    // implementer le constructeur(ajouter ses parametres) et les autresz methodes
-    public Client(){
-        
+    // Constructeur
+    public Client(String codeClient, String nom, String prenom, String telephone, String courriel, int numeroNIP){
+        this.codeClient = codeClient;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.telephone = telephone;
+        this.courriel = courriel;
+        this.numeroNIP = numeroNIP;
+    }
+
+    // Getters
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getCodeClient() {
+        return codeClient;
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return "Nom : " + nom + ", Prenom : " +  prenom + ", Numéro de téléphone :  " + telephone + ", Adresse Courriel : " + courriel + ", Code NIP : " + numeroNIP;
     }
 }
