fdc8a38f37d7f9542ed97e5547628f0018530972
2372559 <2372559@crosemont.qc.ca>
2024-10-24 19:51:31 -0400

    Implémentation de la classe transaction

--- Transaction.java
+++ Transaction.java
@@ -1,13 +1,23 @@
 public class Transaction {
+
     private int numeroTransaction;
     private double montant;
     private Compte compte;
     private Compte compteTransfert;
+    private String type;
 
-    public Transaction(int numeroTransaction, double montant, Compte compte, Compte compteTransfert){
+    // Constructeur
+
+    public Transaction(int numeroTransaction, double montant, Compte compte, Compte compteTransfert, String type){
         this.numeroTransaction = numeroTransaction;
         this.montant = montant;
         this.compte = compte;
         this.compteTransfert = compteTransfert;
+        this.type = type;
+    }
+
+    // toString 
+    public String toString() {
+        return "Numéro de transaction : " + numeroTransaction + ", Montant : " + montant + ", Compte : " + compte + ", Compte à transferer : " + compteTransfert + ", Type de transaction : " + type;
     }
 }
