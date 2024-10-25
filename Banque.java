43245bfcc8ff2b88a007896ccf5325d292ecaa0d
2372559 <2372559@crosemont.qc.ca>
2024-10-24 20:04:55 -0400

    Implémentation de la classe Banque sauf pour remplirGuichet()

--- Banque.java
+++ Banque.java
@@ -1,3 +1,21 @@
 public class Banque {
-    
+
+    private double montantMaximum;
+    private double montantRemplissage;
+
+    // Constructeur
+    public Banque(double montantMaximum, double montantRemplissage) {
+        this.montantMaximum = montantMaximum;
+        this.montantRemplissage = montantRemplissage;
+    }
+
+    // Remplir Guichet 
+    public void remplirGuichet() {
+
+    }
+
+    // toString 
+    public String toString() {
+        return "Montant Maximum : " + montantMaximum + ", Montant Remplissage : " + montantRemplissage;
+    }
 }
