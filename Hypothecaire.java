047638b4f06d9f6034d21ecf50e4c0ba3c845f1f
Allag Sami Riadh <2372559@crosemont.qc.ca>
2024-10-29 16:33:56 -0400

    Implémentation de la class Hypothecaire

--- Hypothecaire.java
+++ Hypothecaire.java
@@ -1,3 +1,10 @@
-public class Hypothecaire {
+public class Hypothecaire extends Compte {
+
+    public Hypothecaire(int numeroCompte, int codeClient, double soldeCompte, double retraitMaximum, double montantTransfertMaximum) {
+        super(numeroCompte, codeClient, soldeCompte, retraitMaximum, montantTransfertMaximum);
+    }
     
+    public String toString() {
+        return super.toString();
+    }
 }
