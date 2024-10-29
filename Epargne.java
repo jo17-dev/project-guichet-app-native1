8d44a44a2e89d324b757b781f86eb2fba61fa618
jo17-dev <procompter1@gmail.com>
2024-10-29 16:12:08 -0400

    ajout de Epargn.java 2

--- Epargne.java
+++ Epargne.java
@@ -1,3 +1,14 @@
-public class Epargne {
-    
+public class Epargne extends Compte {
+    private double tauxInteret; // doit etre entre 0 et 100 !
+
+
+    public Epargne(int numeroCompte, int codeClient, double soldeCompte, double retraitMaximum, double montantTransfertMaximum){
+        super(numeroCompte, codeClient, soldeCompte, retraitMaximum, montantTransfertMaximum);// montantRetrait=1000
+    }
+
+    public void paiementInteret(){
+        // retrait(getSolde() * tauxInteret);
+        setSolde(getSolde()- getSolde()*tauxInteret/100);
+    }
+
 }
