8b1e7df94ccf5dcc1bdea597a8c3e653b74ddf09
Allag Sami Riadh <2372559@crosemont.qc.ca>
2024-10-29 15:44:59 -0400

    Implementation de la classe marge

--- Marge.java
+++ Marge.java
@@ -1,3 +1,20 @@
-public class Marge {
+public class Marge extends Compte {
+
+    private double tauxInteret;
     
+    // Constructeur
+    public Marge(double tauxInteret, int numeroCompte, int codeClient, double soldeCompte, double retraitMaximum, double montantTransfertMaximum) {
+        super(numeroCompte, codeClient, soldeCompte, retraitMaximum, montantTransfertMaximum);
+        this.tauxInteret = tauxInteret;
+    }
+
+    // Augumenter solde marge
+    public void augumenterSoldeMarge(double pourcentage) {
+        solde += (solde*(pourcentage/100));
+    }
+
+    // ToString
+    public String toString() {
+        return super.toString() + "Taux D'interet : " + tauxInteret;
+    }
 }
