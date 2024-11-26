0c5d2ed49656024b5dba271f8d19b0e7ca63c2ae
2372559 <2372559@crosemont.qc.ca>
2024-11-25 20:22:56 -0500

    Remplir guichet implémentée

--- src/main/java/backend/Banque.java
+++ src/main/java/backend/Banque.java
@@ -4,20 +4,27 @@ public class Banque extends Compte {
 
     private double montantMaximum;
     private double montantRemplissage;
+    private double montantTotal;
 
     // Constructeur
     public Banque(int numeroCompte, int codeClient, double soldeCompte, double retraitMaximum, double montantTransfertMaximum, double montantMaximum, double montantRemplissage) {
         super(numeroCompte, codeClient, soldeCompte, retraitMaximum, montantTransfertMaximum);
         this.montantMaximum = montantMaximum;
         this.montantRemplissage = montantRemplissage;
+        this.montantTotal = montantTotal;
     }
 
     // Remplir Guichet 
     public void remplirGuichet() {
+        if (montantRemplissage + montantTotal > montantMaximum)  {
+            System.out.println("Echec. Le montant dépasse le montant maximum");
+        } else {
+            montantTotal += montantRemplissage;
+        }
     }
 
     // toString 
     public String toString() {
-        return "Montant Maximum : " + montantMaximum + ", Montant Remplissage : " + montantRemplissage;
+        return "Montant Total : " + montantTotal + "Montant Maximum : " + montantMaximum + ", Montant Remplissage : " + montantRemplissage;
     }
 }
